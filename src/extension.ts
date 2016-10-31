e345a437e1b3a22abcf400d0b5b82372436fb1e7
Darin Morrison <freebroccolo@users.noreply.github.com>
2016-10-31 01:16:11 -0600

    Cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -30,10 +30,10 @@ class Session {
     return this;
   }
 
-  async execute(fileName: string): Promise<null | string> {
+  execute(fileName: string): Promise<null | string> {
     if (this.config.path == null) {
       vscode.window.showWarningMessage(`The RedPRL binary path needs to be configured. See the "redprl.path" setting.`);
-      return null;
+      return Promise.resolve(null);
     };
     const child = childProcess.spawn(this.config.path, [fileName]);
     return new Promise<null | string>((resolve) => {
@@ -42,8 +42,7 @@ class Session {
       child.on("close", (code: number) => code === 0 || code === 1 ? resolve(buffer) : resolve(null));
       child.on("error", (error: Error & { code: string }) => {
         if (error.code === "ENOENT") {
-          const msg = `Cannot find redprl binary at "${this.config.path}".`;
-          vscode.window.showWarningMessage(msg);
+          vscode.window.showWarningMessage(`Cannot find redprl binary at "${this.config.path}".`);
           vscode.window.showWarningMessage(`Double check your path or try configuring "redprl.path" under "User Settings".`);
         }
       });
@@ -58,21 +57,10 @@ class Session {
   // NOTE: some of this data should probably be displayed in a virtual document in a separate panel
   async refreshDiagnostics(response: string): Promise<void> {
     this.diagnostics.clear();
-
     let collated: Map<vscode.Uri, vscode.Diagnostic[]> = new Map();
-    let diagnostics: vscode.Diagnostic[] = [];
-
-    let uri: vscode.Uri;
-    let startLine = NaN;
-    let startChar = NaN;
-    let endLine = NaN;
-    let endChar = NaN;
-
     let matchDiagnostic: null | RegExpExecArray = null;
-
     while ((matchDiagnostic = Pattern.diagnostic.exec(response)) != null) {
       matchDiagnostic.shift();
-      // this.output.appendLine(JSON.stringify(matchDiagnostic));
       const [path, startLineRaw, startCharRaw, endLineRaw, endCharRaw, kind, message] = matchDiagnostic;
       let severity: null | vscode.DiagnosticSeverity = null;
       switch (kind) {
@@ -87,13 +75,14 @@ class Session {
           break;
       }
       if (severity != null) {
-        try { uri = vscode.Uri.parse(`file://${path}`); } catch (err) { continue; }
-        if (isNaN((startLine = parseInt(startLineRaw)))) continue;
-        if (isNaN((startChar = parseInt(startCharRaw)))) continue;
-        if (isNaN((  endLine = parseInt  (endLineRaw)))) continue;
-        if (isNaN((  endChar = parseInt  (endCharRaw)))) continue;
+        let uri: vscode.Uri;
+        try { uri = vscode.Uri.parse(`file://${path}`) } catch (err) { continue }
+        const startLine = parseInt(startLineRaw);
+        const startChar = parseInt(startCharRaw);
+        const   endLine = parseInt(  endLineRaw);
+        const   endChar = parseInt(  endCharRaw);
         if (!collated.has(uri)) collated.set(uri, []);
-        diagnostics = collated.get(uri) as vscode.Diagnostic[];
+        const diagnostics = collated.get(uri) as vscode.Diagnostic[];
         const range = new vscode.Range(startLine, startChar, endLine, endChar);
         const entry = new vscode.Diagnostic(range, message, severity);
         diagnostics.push(entry);
@@ -104,20 +93,23 @@ class Session {
   }
 }
 
+function onDidChangeConfiguration(session: Session): () => void {
+  return () => (session as any).config = vscode.workspace.getConfiguration("redprl");
+}
+
+function onDidSaveTextDocument(session: Session): (textDocument: vscode.TextDocument) => void {
+  return (textDocument) => {
+    if (textDocument.languageId !== "redprl") return;
+    if (session.config.enableDiagnosticsOnSave) session.refresh(textDocument);
+  };
+}
+
 export function activate(context: vscode.ExtensionContext) {
   const session = new Session();
   context.subscriptions.push(vscode.languages.setLanguageConfiguration("redprl", configuration));
   context.subscriptions.push(vscode.commands.registerTextEditorCommand("redprl.refreshDiagnostics", (editor) => session.refresh(editor.document)));
-  vscode.workspace.onDidChangeConfiguration(() => {
-    (session as any).config = vscode.workspace.getConfiguration("redprl");
-  });
-  vscode.workspace.onDidSaveTextDocument((textDocument) => {
-    if (textDocument.languageId === "redprl") {
-      if (session.config.enableDiagnosticsOnSave) {
-        session.refresh(textDocument);
-      }
-    }
-  });
+  vscode.workspace.onDidChangeConfiguration(onDidChangeConfiguration(session));
+  vscode.workspace.onDidSaveTextDocument(onDidSaveTextDocument(session));
 }
 
 export function deactivate() {
